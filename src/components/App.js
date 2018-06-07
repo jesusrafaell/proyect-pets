f50a2d503bc818fa1d69a8bb0bad27df1bc33cc0
Jesus Rafaell <37907710+jesusrafaell@users.noreply.github.com>
2018-06-06 21:35:57 -0400

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Navbar from '../components/Common/Navbar';
 import axios from 'axios';
